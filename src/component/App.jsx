0ad86c7a1c36d152df52e00c7e7c470b949eaf23
unknown <sumitkumar9006@gmail.com>
2020-10-30 15:46:19 +0530

    starting make a wish

---
+++ src/component/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div >
+      hello
+    </div>
+  );
+}
+
+export default App;
